12b4d2dd57f3ca8b53dfa382870be68d822f25e5
SangJune-Kim <sangjunekim@SangJuneui-MacBookAir.local>
2022-03-14 18:26:02 +0900

    첫 잔디

---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,29 @@
+import {MyTitle,MyEmail,Wrapper} from '../../styles/emotion'
+
+
+
+export default function AAApage() {
+
+
+    // 여기는 자바스크립트 쓰는 곳//
+
+    return (
+        //html 쓰는곳 //
+        <Wrapper>
+        <MyTitle>
+             로그인
+             <br></br>
+             <br></br>
+             아이디
+             <br></br>
+            <MyEmail type="text"/>
+            <br></br>
+            비밀번호 
+            <br></br>
+            <MyEmail type="text"/>
+        </MyTitle>
+        </Wrapper>
+       
+   
+  )
+}
---
+++ freeboard_frontend/styles/NewBoards.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+export const MyTitle = styled.div`
+`
+export const MyEmail = styled.input`
+    width: 200px;
+`
+export const Wrapper = styled.div`
+width: 1200px; height: auto; padding: 50px 500px;
+display: flex; flex-direction: row
+
+`
