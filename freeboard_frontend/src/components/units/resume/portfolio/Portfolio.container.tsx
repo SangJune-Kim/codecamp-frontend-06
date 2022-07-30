87ad93887e12a66e17dd26409bf2eb879fe4a670
SangJune-Kim <ghfnsgldl@gmail.com>
2022-07-30 20:02:23 +0900

    0730

---
+++ freeboard_frontend/src/components/units/resume/portfolio/Portfolio.container.tsx
@@ -0,0 +1,10 @@
+import PortfolioUI from "./Portfolio.presenter";
+
+const Portfolio = () => {
+  return (
+    <>
+      <PortfolioUI />
+    </>
+  );
+};
+export default Portfolio;
---
+++ freeboard_frontend/src/components/units/resume/portfolio/Portfolio.presenter.tsx
@@ -0,0 +1,10 @@
+import * as P from "./Portfolio.styles";
+
+const PortfolioUI = () => {
+  return (
+    <P.Container>
+      <div></div>
+    </P.Container>
+  );
+};
+export default PortfolioUI;
